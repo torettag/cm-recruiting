819b76e90b80a2bf6de0dd1f4218ce53097a8a3c
Joseph Tresca <jtresca@glgroup.com>
2017-06-23 10:29:09 -0400

    added icons to activity monitor for each event type.

--- src/components/Activitymon.js
+++ src/components/Activitymon.js
@@ -16,12 +16,31 @@ class Activitymon extends React.Component {
   componentDidMount () {
     let activity = this.props.referralsStore.activity;
 
+
+    //we want to sort activity starting at most recent
     activity = activity.sort((a,b) => {
       return new Date(b.activityDate).getTime() - new Date(a.activityDate).getTime()
     })
 
+
+    //hash table for swapping in the correct icon
+    let classAssign = {
+     "Invited" : "icon-invite_icon",
+     "Accepted": "icon-accepted_icon",
+     "Signed Terms": "icon-signed_icon",
+     "Given to Client" : "icon-give_icon",
+     "Paid" : "icon-paid_icon"
+    }
+
+    //need to append the icon's classname to each object
+    activity.forEach((act) => {
+      let assignment = classAssign[act.activity];
+      act.classNamed = assignment;
+    })
+
+    //we store the final activity object here
     this.setState({ activity });
-    console.log("ACTIVITY?: ", activity);
+      console.log("ACTIVITY?: ", activity);
   }
 
 
@@ -39,7 +58,7 @@ class Activitymon extends React.Component {
                       return (
                         <section key={i} id='activitytextblock'>
                           <div className='flex-row'>
-                            <div className='circle'></div><span className='activitytext'>Your referral {event.firstName} {event.lastName} has a status update: {event.activity}<span className='activitytime'>{moment(event.activityDate).fromNow()}</span></span>
+                            <span className={event.classNamed}></span><span className='activitytext'>Your referral {event.firstName} {event.lastName} has a status update: {event.activity}<span className='activitytime'>{moment(event.activityDate).fromNow()}</span></span>
                           </div>
                         </section>
                       );
